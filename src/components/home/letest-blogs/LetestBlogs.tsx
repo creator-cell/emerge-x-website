932ea5d937e5b82e20645dee12486f501857f15f
akshitbusa <akshitbusa2002@gmail.com>
2024-12-07 14:53:37 +0530

    latest blog

--- src/components/home/letest-blogs/LetestBlogs.tsx
+++ src/components/home/letest-blogs/LetestBlogs.tsx
@@ -60,12 +60,12 @@ const LetestBlogs: React.FC = () => {
           newWidth = 96; // Width starts at 20%, increases to 100%
           newRadius = Math.max(0, 70 - visibility * 50);
         } else if (visibility >= 0.28403659666684005) {
-          newWidth = 75;
-        }else if (visibility >= 0.21403659666684005) {
-          newWidth = 60;
-        }else if (visibility >= 0.16403659666684005) {
+          newWidth = 80;
+        } else if (visibility >= 0.21403659666684005) {
+          newWidth = 70;
+        } else if (visibility >= 0.16403659666684005) {
           newWidth = 62;
-        }else if (visibility >= 0.12403659666684005) {
+        } else if (visibility >= 0.12403659666684005) {
           newWidth = 50;
         }
 
@@ -92,45 +92,50 @@ const LetestBlogs: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex justify-center ">
-      <div
-        className="bg-white border-transparent"
-        id="latest-blogs-section"
-        style={{
-          width: width,
-          borderTopLeftRadius: borderRadius,
-          borderTopRightRadius: borderRadius,
-          transition: "width 0.6s ease-out, border-radius 0.6s ease-out", // Smoother and slower transition
-        }}
-      >
-        <SectionWrapper>
-          <SectionHeading text="Latest Blogs" className="text-black mt-64" />
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-[70px] gap-y-10 lg:gap-x-[110px] mt-8 md:mt-14 lg:mt-16">
-            {letestBlogData.map((e, i) => (
-              <div key={i} className="text-center flex flex-col items-center md:gap-4 py-8">
-                <div className="w-full rounded-[20px] overflow-hidden">
-                  <Image
-                    src={e.img}
-                    alt="services_images"
-                    height={600}
-                    width={600}
-                  />
-                </div>
+    <>
+      <div>
 
-                <div className="flex items-center justify-between w-full px-2 mt-7 md:mt-10 gap-4">
-                  <h3 className="text-black text-xl md:text-2xl text-start font-semibold w-[70%]">
-                    {e.title}
-                  </h3>
-                  <button className="rounded-full text-sm md:text-base w-[102px] py-1.5 md:py-2 bg-customGreen text-white font-semibold">
-                    View more
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </SectionWrapper>
       </div>
-    </div>
+      <div className="flex justify-center ">
+        <div
+          className="bg-white border-transparent"
+          id="latest-blogs-section"
+          style={{
+            width: width,
+            borderTopLeftRadius: borderRadius,
+            borderTopRightRadius: borderRadius,
+            transition: "width 0.6s ease-out, border-radius 0.6s ease-out", // Smoother and slower transition
+          }}
+        >
+          <SectionWrapper>
+            <SectionHeading text="Latest Blogs" className="text-black mt-48" />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-[70px] gap-y-10 lg:gap-x-[110px] mt-8 md:mt-14 lg:mt-16">
+              {letestBlogData.map((e, i) => (
+                <div key={i} className="text-center flex flex-col items-center md:gap-4 py-8">
+                  <div className="w-full rounded-[20px] overflow-hidden">
+                    <Image
+                      src={e.img}
+                      alt="services_images"
+                      height={600}
+                      width={600}
+                    />
+                  </div>
+
+                  <div className="flex items-center justify-between w-full px-2 mt-7 md:mt-10 gap-4">
+                    <h3 className="text-black text-xl md:text-2xl text-start font-semibold w-[70%]">
+                      {e.title}
+                    </h3>
+                    <button className="rounded-full text-sm md:text-base w-[102px] py-1.5 md:py-2 bg-customGreen text-white font-semibold">
+                      View more
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </SectionWrapper>
+        </div>
+      </div>
+    </>
   );
 };
 
