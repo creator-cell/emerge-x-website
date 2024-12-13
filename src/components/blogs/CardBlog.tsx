80c758d0c00b4654f475f9126b287ef15b7a38f7
ankushw1 <waghmareankush050@gmail.com>
2024-12-13 10:25:20 +0530

    blog card changes

--- src/components/blogs/CardBlog.tsx
+++ src/components/blogs/CardBlog.tsx
@@ -5,7 +5,6 @@ import React from "react";
 import SubtractIcon from "@";
 import Image from "next/image";
 
-
 interface CardBlogTypes {
   list: boolean;
   styleHeading?: string;
@@ -30,24 +29,24 @@ const CardBlog: React.FC<CardBlogTypes> = ({
     >
       <div
         className={cn(
-          ` ${list ? "w-[30%] " : " w-full "}  rounded-[14px] bg-[#D9D9D9] relative`,
+          ` ${
+            list ? "w-[30%] " : " w-full "
+          }  rounded-[14px] bg-[#D9D9D9] relative`,
           styleBox
         )}
       >
-           <Image
-  src="/blogs/Subtract.svg"
-  alt="Subtract Icon"
-  width={100} // Set the actual width in pixels
-  height={100} // Set the actual height in pixels
-  className="absolute top-0 left-0"
-/>
-<Image
-          src="/blogs/button.png"
-          width={80}
-          height={80}
-          alt="Button Icon"
-          className="absolute top-[1px] left-[5px] "
+        <Image
+          src="/blogs/Subtract.svg"
+          alt="Subtract Icon"
+          width={100} // Set the actual width in pixels
+          height={100} // Set the actual height in pixels
+          className="absolute top-0 left-0"
         />
+      <button 
+  className="absolute top-[1px] left-[7px] w-[80px] h-[25px] bg-green-500 text-white rounded-tl-[10px] rounded-br-[15px]">
+  Ankush
+</button>
+
       </div>
       <div className={`${list ? " w-[60%] h-fit" : "w-full"}`}>
         <p className={cn(" text-customGreen text-xs font-[500]", styleDate)}>
